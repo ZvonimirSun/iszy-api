528a26e0f363e899d20ec56a5b5ec071ec16e8e3
ZvonimirSun <hi@iszy.xyz>
2021-07-28 15:30:54 +0800

    feat(role): 添加权限，测试多对多关系表

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './entities/user.model';
+import { Role } from './entities/role.model';
+import { UserRole } from './entities/user_role.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, Role, UserRole])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
