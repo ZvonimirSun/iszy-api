9302547fc4d248a86d40009b7c46b4aa62cb5a1d
ZvonimirSun <hi@iszy.xyz>
2022-09-30 16:23:38 +0800

    feat(user): 调整注册逻辑

--- src/modules/auth/modules/user/user.service.ts
+++ src/modules/auth/modules/user/user.service.ts
@@ -3,16 +3,31 @@ import { User } from './entities/user.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { Role } from './entities/role.model';
 import { Privilege } from './entities/privilege.model';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectModel(User)
-    private userModel: typeof User,
+    @InjectModel(User) private userModel: typeof User,
+    private sequelize: Sequelize,
   ) {}
 
-  async create(user: Partial<User>) {
-    return this.userModel.create(user);
+  async create(user: Partial<User>, userId?: number): Promise<User> {
+    return await this.sequelize.transaction(async (t) => {
+      const transactionHost = { transaction: t };
+      if (userId != null) {
+        return await this.userModel.create(
+          { ...user, userId },
+          transactionHost,
+        );
+      } else {
+        const userEntity = await this.userModel.create(user, transactionHost);
+        return await userEntity.update({
+          createBy: userEntity.id,
+          updateBy: userEntity.id,
+        });
+      }
+    });
   }
 
   async findOne(key: string): Promise<User> {
