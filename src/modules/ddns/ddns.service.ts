44daa569427cdbdbdcc1f0795dff1b3a07722daf
ZvonimirSun <hi@iszy.xyz>
2025-03-31 10:57:12 +0800

    feat(ddns): 添加阿里云dns

--- src/modules/ddns/ddns.service.ts
+++ src/modules/ddns/ddns.service.ts
@@ -1,3 +1,6 @@
+import Alidns20150109, * as $Alidns20150109 from '@alicloud/alidns20150109'
+import * as $OpenApi from '@alicloud/openapi-client'
+import * as $Util from '@alicloud/tea-util'
 import { Injectable, Logger } from '@nestjs/common'
 import axios from 'axios'
 import { DDNSUpdateDto } from './dto/ddns_update.dto'
@@ -18,7 +21,72 @@ export class DDNSService {
   }
 
   async updateAli(query: DDNSUpdateDto) {
-    return 'test'
+    const { hostname, ip, username: accessKeyId, password: accessKeySecret } = query
+
+    let keyword = ''
+    let domainName = ''
+    const tmp = hostname.split('.')
+    if (tmp.length < 2) {
+      return 'badauth'
+    }
+    else if (tmp.length === 2) {
+      keyword = '@'
+      domainName = tmp.join('.')
+    }
+    else {
+      keyword = tmp.slice(0, -2).join('.')
+      domainName = tmp.slice(-2).join('.')
+    }
+
+    // Endpoint 请参考 https://api.aliyun.com/product/Alidns
+    const endPoint = `dns.aliyuncs.com`
+    const config = new $OpenApi.Config({
+      accessKeyId,
+      accessKeySecret,
+    })
+    config.endpoint = endPoint
+    const client = new Alidns20150109(config)
+
+    try {
+      const describeDomainRecordsRequest = new $Alidns20150109.DescribeDomainRecordsRequest({
+        domainName,
+        typeKeyWord: 'A',
+        RRKeyWord: keyword,
+      })
+      const runtime = new $Util.RuntimeOptions({ })
+      const result = await client.describeDomainRecordsWithOptions(describeDomainRecordsRequest, runtime)
+      if (!result.body.domainRecords.record.length) {
+        const addDomainRecordRequest = new $Alidns20150109.AddDomainRecordRequest({
+          domainName,
+          RR: keyword,
+          type: 'A',
+          value: ip,
+        })
+        await client.addDomainRecordWithOptions(addDomainRecordRequest, runtime)
+        return 'good'
+      }
+      else {
+        const recordId = result.body.domainRecords.record[0].recordId
+        const currentIP = result.body.domainRecords.record[0].value
+        if (currentIP === ip) {
+          return 'nochg'
+        }
+        const updateDomainRecordRequest = new $Alidns20150109.UpdateDomainRecordRequest({
+          recordId,
+          RR: keyword,
+          type: 'A',
+          value: ip,
+        })
+        await client.updateDomainRecordWithOptions(updateDomainRecordRequest, runtime)
+        return 'good'
+      }
+    }
+    catch (error) {
+      this.logger.error(error.name)
+      this.logger.error(error.message)
+      this.logger.error(error.data.Recommend)
+      return 'badauth'
+    }
   }
 
   async updateCloudflare(query: DDNSUpdateDto) {
