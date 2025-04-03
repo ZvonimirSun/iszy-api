f0c012d1954217db272e85d70a090f6b8407a8c5
ZvonimirSun <hi@iszy.xyz>
2025-04-03 12:34:07 +0800

    fix: 登录接口补充设备信息

--- src/modules/core/auth/local.strategy.ts
+++ src/modules/core/auth/local.strategy.ts
@@ -1,20 +1,28 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, Req, UnauthorizedException } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-local'
+import { AuthRequest } from '~types/AuthRequest'
 import { AuthService } from './auth.service'
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super()
+    super({
+      passReqToCallback: true,
+    })
   }
 
   async validate(
+    @Req() req: AuthRequest,
     username: string,
     password: string,
   ): Promise<any> {
     try {
-      return await this.authService.validateUser(username, password)
+      const user = await this.authService.validateUser(username, password)
+      req.device = {
+        ip: req.ip,
+      }
+      return user
     }
     catch (e) {
       throw new UnauthorizedException(e)
