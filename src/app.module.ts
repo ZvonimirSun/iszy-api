2dd4a588d0541187a9681bb411e15896bb7ea81a
zvonimirsun <hi@iszy.xyz>
2025-04-06 21:45:56 +0800

    chore: 调整模组依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
-import { ConnectionModule } from '~modules/core/connection/connection.module'
 import { CoreModule } from '~modules/core/core.module'
 import { DDNSModule } from '~modules/ddns/ddns.module'
 import { GisModule } from '~modules/gis/gis.module'
 import { HolidayModule } from '~modules/holiday/holiday.module'
+import { IdiomModule } from '~modules/idiom/Idiom.module'
 import { IszyToolsModule } from '~modules/iszy_tools/iszy_tools.module'
+import { JsoneditorModule } from '~modules/jsonEditor/jsoneditor.module'
 import { MockModule } from '~modules/mocks/mock.module'
 import { UrlsModule } from '~modules/urls/urls.module'
 import { AppController } from './app.controller'
@@ -13,13 +14,14 @@ import { AppService } from './app.service'
 @Module({
   imports: [
     CoreModule,
-    HolidayModule,
-    IszyToolsModule,
-    GisModule,
-    UrlsModule,
-    ConnectionModule,
-    MockModule,
     DDNSModule,
+    GisModule,
+    HolidayModule,
+    IdiomModule,
+    IszyToolsModule,
+    JsoneditorModule,
+    MockModule,
+    UrlsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/iszy_tools/iszy_tools.module.ts
+++ src/modules/iszy_tools/iszy_tools.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Settings } from '~entities/iszy_tools/settings.model'
-import { IdiomModule } from '~modules/idiom/Idiom.module'
-import { JsoneditorModule } from '~modules/jsonEditor/jsoneditor.module'
 import { IszyToolsController } from './iszy_tools.controller'
 import { IszyToolsService } from './iszy_tools.service'
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Settings]),
-    IdiomModule,
-    JsoneditorModule,
   ],
   controllers: [IszyToolsController],
   providers: [IszyToolsService],
