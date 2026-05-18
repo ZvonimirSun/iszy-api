94b2623060acb2975d009df4174ca85264491916
ZvonimirSun <hi@iszy.xyz>
2026-05-18 23:01:19 +0800

    feat: 隐私保护，只记录低精度ip

--- src/domains/auth/utils/generateDevice.ts
+++ src/domains/auth/utils/generateDevice.ts
@@ -1,15 +1,46 @@
 import type { AuthRequest } from '~shared'
 import { Device } from '@zvonimirsun/iszy-common'
+import ip from 'ip'
 import { UAParser } from 'ua-parser-js'
 
 export function generateDevice(req: AuthRequest): Device {
   const ua = req.header('user-agent')
   // 获取设备名称，如 Chrome 103 on Windows 11
   const data = UAParser(ua)
+  const dataType: {
+    [key in UAParser.IDevice['type']]?: string
+  } = {
+    desktop: 'PC',
+    mobile: '手机',
+    tablet: '平板',
+    console: '主机',
+    smarttv: '智能电视',
+    wearable: '手表',
+    xr: 'VR',
+    embedded: '嵌入式设备',
+  }
   return {
-    name: `${data.os.name} · ${data.browser.name}`,
-    ip: req.ip,
+    name: dataType[data.device.type || 'desktop'] || '未知设备',
+    ip: maskIp(req.ip),
     createdAt: new Date(),
     updatedAt: new Date(),
+  }
+}
+
+function maskIp(address: string) {
+  if (ip.isV4Format(address)) {
+    if (ip.isV4Format(address)) {
+      // IPv4 /24
+      const network = ip.cidrSubnet(`${address}/24`).networkAddress
+      return `${network}/24`
+    }
+    else if (ip.isV6Format(address)) {
+      // IPv6 /64
+      const network = ip.cidrSubnet(`${address}/64`).networkAddress
+      return `${network}/64`
+    }
+    else {
+      throw new Error('Invalid IP address')
+    }
   }
 }
